0fe5a693e1d52c08c6c0b5afc0f39ab688d16ea6
koni-v <k.vidovic.mk@gmail.com>
2024-06-23 22:50:07 +0200

    Player Creation

--- index.js
+++ index.js
@@ -9,8 +9,24 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 
 const image = new Image() // html image object
 image.src = './img/Pallet Town.png'
-console.log(image)
+
+const playerImage = new Image()
+playerImage.src = './img/playerDown.png'
 
 image.onload = () => {
-    c.drawImage(image, -750, -400)
+    c.drawImage(image, -865, -560)
+    c.drawImage(
+        playerImage,
+        0, // x coordinate for the crop 
+        0, // y coordinate for the crop 
+        playerImage.width / 4, // crop width
+        playerImage.height, // crop height
+        canvas.width / 2 - playerImage.width / 2, // x coordinate on canvas
+        canvas.height / 2 - playerImage.height / 2, // y coordinate on canvas
+        playerImage.width / 4, // croped image width
+        playerImage.height // croped image height
+    )
 }
+
+
+
